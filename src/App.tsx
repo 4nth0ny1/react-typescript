202e3ad8ead4a693f526a7a5ee6faaebe1ed0fb3
4nth0ny1 <acatullo4@gmail.com>
2023-08-02 15:47:49 -0500

    reset app component

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,11 @@
 import React from "react";
 import "./App.css";
-import { Greet } from "./components/Greet";
-import { Person } from "./components/Person";
-import { PersonList } from "./components/PersonList";
-import { Status } from "./components/Status";
-import { Heading } from "./components/Heading";
-import { Oscar } from "./components/Oscar";
-import { Button } from "./components/Button";
-import { Input } from "./components/Input";
 
 function App() {
-  const personName = {
-    first: "John",
-    last: "Joe",
-  };
-
-  const nameList = [
-    { first: "Bill", last: "Johnson" },
-    { first: "Steve", last: "Kent" },
-    { first: "Alicea", last: "Miller" },
-  ];
-
   return (
     <div className="App">
       <h1>React Typescript Demo</h1>
       <hr></hr>
-      <Input value="" handleChange={(event) => console.log(event)} />
-      <Button
-        handleClick={(event, id) => console.log("Button Clicked", event, id)}
-      />
     </div>
   );
 }
