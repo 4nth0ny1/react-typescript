5b2a4b563ddca0ef67c16439d9f9b182d0a612e0
4nth0ny1 <acatullo4@gmail.com>
2023-08-02 16:00:01 -0500

    added state component tsx LoggedIn

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import React from "react";
 import "./App.css";
+import { LoggedIn } from "./components/hooks/LoggedIn";
 
 function App() {
   return (
     <div className="App">
       <h1>React Typescript Demo</h1>
       <hr></hr>
+      <LoggedIn />
     </div>
   );
 }
