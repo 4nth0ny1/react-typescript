995370854e95d8220baf62c125ab86a59857ea5c
4nth0ny1 <acatullo4@gmail.com>
2023-08-02 16:08:53 -0500

    added more complex user state with <AppUser | null>

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import "./App.css";
 import { LoggedIn } from "./components/hooks/LoggedIn";
+import { User } from "./components/hooks/User";
 
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
       <h1>React Typescript Demo</h1>
       <hr></hr>
       <LoggedIn />
+      <User />
     </div>
   );
 }
