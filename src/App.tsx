7a3b044681f3cb378c1f3d89c030bfd17fb4a7e8
4nth0ny1 <acatullo4@gmail.com>
2023-08-02 18:28:10 -0500

    reset app component

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,11 @@
 import React from "react";
 import "./App.css";
-import { LoggedIn } from "./components/hooks/LoggedIn";
-import { User } from "./components/hooks/User";
 
 function App() {
   return (
     <div className="App">
       <h1>React Typescript Demo</h1>
       <hr></hr>
-      <LoggedIn />
-      <User />
     </div>
   );
 }
