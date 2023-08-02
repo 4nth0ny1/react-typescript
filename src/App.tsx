a953e82836a7f1b587872e4165c2c995f4390aae
4nth0ny1 <acatullo4@gmail.com>
2023-08-02 18:39:20 -0500

    reset app comp

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,11 @@
 import React from "react";
 import "./App.css";
-import { Private } from "./components/auth/Private";
-import { Profile } from "./components/auth/Profile";
 
 function App() {
   return (
     <div className="App">
       <h1>React Typescript Demo</h1>
       <hr></hr>
-      <Private isLoggedIn={false} component={Profile} />
     </div>
   );
 }
