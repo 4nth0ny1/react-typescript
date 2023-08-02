e95475337995865ef0586bc6a513cae0db070039
4nth0ny1 <acatullo4@gmail.com>
2023-08-02 15:10:26 -0500

    basic typescript props

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,27 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import { Greet } from "./components/Greet";
+import { Person } from "./components/Person";
+import { PersonList } from "./components/PersonList";
 
 function App() {
+  const personName = {
+    first: "John",
+    last: "Joe",
+  };
+
+  const nameList = [
+    { first: "Bill", last: "Johnson" },
+    { first: "Steve", last: "Kent" },
+    { first: "Alicea", last: "Miller" },
+  ];
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>React Typescript Demo</h1>
+      <Greet name="Anthony" messageCount={5} isLoggedIn={false} />
+      <Person name={personName} />
+      <PersonList names={nameList} />
     </div>
   );
 }
